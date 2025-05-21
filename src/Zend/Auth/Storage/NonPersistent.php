fa5fd056f124c174a7722b58e7e28dd23a305609
Jay Klehr <jay@diablomedia.com>
2025-05-21 09:07:55 -0600

    removing unused throws annotations

--- src/Zend/Auth/Storage/NonPersistent.php
+++ src/Zend/Auth/Storage/NonPersistent.php
@@ -44,7 +44,6 @@ class Zend_Auth_Storage_NonPersistent implements Zend_Auth_Storage_Interface
     /**
      * Returns true if and only if storage is empty
      *
-     * @throws Zend_Auth_Storage_Exception If it is impossible to determine whether storage is empty
      * @return boolean
      */
     public function isEmpty()
@@ -56,7 +55,6 @@ class Zend_Auth_Storage_NonPersistent implements Zend_Auth_Storage_Interface
      * Returns the contents of storage
      * Behavior is undefined when storage is empty.
      *
-     * @throws Zend_Auth_Storage_Exception If reading contents from storage is impossible
      * @return mixed
      */
     public function read()
@@ -68,7 +66,6 @@ class Zend_Auth_Storage_NonPersistent implements Zend_Auth_Storage_Interface
      * Writes $contents to storage
      *
      * @param  mixed $contents
-     * @throws Zend_Auth_Storage_Exception If writing $contents to storage is impossible
      * @return void
      */
     public function write($contents)
@@ -79,7 +76,6 @@ class Zend_Auth_Storage_NonPersistent implements Zend_Auth_Storage_Interface
     /**
      * Clears contents from storage
      *
-     * @throws Zend_Auth_Storage_Exception If clearing contents from storage is impossible
      * @return void
      */
     public function clear()
